952173a12b4b62f514f34ee618dab57c0f20066b
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2016-10-31 23:56:51 +0200

    upd font

--- html.js
+++ html.js
@@ -16,7 +16,6 @@ module.exports = React.createClass({
     render() {
         const {body, route} = this.props
         const title = DocumentTitle.rewind()
-        const font = <link href='https://fonts.googleapis.com/css?family=Montserrat|Open+Sans|PT+Serif:400italic&subset=latin,cyrillic' rel='stylesheet' type='text/css' />
 
         let css
         if (process.env.NODE_ENV === 'production') {
@@ -29,10 +28,7 @@ module.exports = React.createClass({
                     <meta charSet="utf-8" />
                     <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
                     <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=5.0" />
-                    <title>
-                        { title }
-                    </title>
-                    { font }
+                    <title>{ title }</title>
                     { css }
                     <link rel="shortcut icon" href={ prefixLink(favicon32) } />
                     <link rel="apple-touch-icon" href={ prefixLink(favicon144) } />
