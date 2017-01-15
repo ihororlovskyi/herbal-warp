5e8ac37c8cce5f418f7b0b46d8b819d654c43b30
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2017-01-15 13:36:25 +0200

    rework MainMenu component

--- components/MainMenu/index.jsx
+++ components/MainMenu/index.jsx
@@ -3,16 +3,13 @@ import { RouteHandler, Link } from 'react-router'
 import { prefixLink } from 'gatsby-helpers'
 import { config } from 'config'
 import Ink from 'react-ink'
+import SocNav from '../SocNav'
 
 import './style.scss'
 import iconSentimonyRecordsLogo from '../../static/img/svg-icons/sentimony-records-logo.svg'
-import iconDiscography from '../../static/img/svg-icons/kokopelli.svg'
-import iconBandcamp from '../../static/img/svg-icons/bandcamp.svg'
-import iconFacebook from '../../static/img/svg-icons/facebook.svg'
-import iconGithub from '../../static/img/svg-icons/github.svg'
-import iconEvevt from '../../static/img/svg-icons/event.svg'
-import iconDiscogs from '../../static/img/svg-icons/discogs.svg'
-import iconYoutube from '../../static/img/svg-icons/youtube.svg'
+import iconReleases from '../../static/img/svg-icons/discogs.svg'
+import iconArtists from '../../static/img/svg-icons/kokopelli.svg'
+import iconEvents from '../../static/img/svg-icons/event.svg'
 
 class MainMenu extends React.Component {
     render() {
@@ -34,41 +31,24 @@ class MainMenu extends React.Component {
                     </Link>
 
                     <Link to={ prefixLink('/releases/')} className='mainmenu__link' activeClassName='is-selected'>
-                        <img className='mainmenu__link-svg' style={{display:'none'}} src={ prefixLink(iconDiscogs) }/>
+                        <img className='mainmenu__link-svg' src={ prefixLink(iconReleases) }/>
                         <span className='mainmenu__link-name'>Releases</span>
                         <Ink />
                     </Link> 
 
                     <Link to={ prefixLink('/artists/')} className={`mainmenu__link ${ location.pathname.indexOf(prefixLink('/artists/')) == 0 ? 'is-selected' : '' }`} style={{display:'none'}}>
-                        <img className='mainmenu__link-svg' style={{display:'none'}} src={ prefixLink(iconDiscography) }/>
+                        <img className='mainmenu__link-svg' style={{display:'none'}} src={ prefixLink(iconArtists) }/>
                         <span className='mainmenu__link-name'>Artists</span>
                         <Ink />
                     </Link> 
 
                     <Link to={ prefixLink('/events/')} className={`mainmenu__link ${ location.pathname.indexOf(prefixLink('/events/')) == 0 ? 'is-selected' : '' }`} style={{display:'none'}}>
-                        <img className='mainmenu__link-svg' style={{display:'none'}} src={ prefixLink(iconEvevt) }/>
+                        <img className='mainmenu__link-svg' style={{display:'none'}} src={ prefixLink(iconEvents) }/>
                         <span className='mainmenu__link-name'>Events</span>
                         <Ink />
                     </Link> 
 
-                    <div className='mainmenu__social-nav'>
-
-                        <a href={ config.siteYoutubeUrl } className='mainmenu__link' target='_blank'>
-                            <img className='mainmenu__link-svg' src={ prefixLink(iconYoutube) }/>
-                            <span className='mainmenu__link-name'>youtube</span>
-                        </a>
-
-                        <a href={ config.siteFacebookUrl } className='mainmenu__link' target='_blank'>
-                            <img className='mainmenu__link-svg' src={ prefixLink(iconFacebook) }/>
-                            <span className='mainmenu__link-name'>facebook</span>
-                        </a>
-
-                        <a href={ config.siteBandcampUrl } className='mainmenu__link' target='_blank'>
-                            <img className='mainmenu__link-svg' src={ prefixLink(iconBandcamp) }/>
-                            <span className='mainmenu__link-name'>bandcamp</span>
-                        </a>
-
-                    </div>
+                    <SocNav {...this.props} />
 
                 </div>
 
