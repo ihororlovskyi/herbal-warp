1f266f5183771317372018793f1ee9cb4564cfc6
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2017-01-15 15:20:18 +0200

    upd Header component

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { RouteHandler, Link } from 'react-router'
 import { prefixLink } from 'gatsby-helpers'
 import { config } from 'config'
 import Ink from 'react-ink'
@@ -11,25 +10,27 @@ import './style.scss'
 
 class Header extends React.Component {
     render() {
-        const {location} = this.props
-        const {route} = this.props
-        const page = route.page.data
 
         return (
             <div className='header'>
                 <div className='header__wrapper'>
-                    <HeaderLogo {...this.props} />
-                    <MainMenu {...this.props} />
-                    <SocNav {...this.props} />
+
+                    <div className='header__header-logo'>
+                        <HeaderLogo {...this.props} />
+                    </div>
+
+                    <div className='header__mainmenu'>
+                        <MainMenu {...this.props} />
+                    </div>
+
+                    <div className='header__socnav'>
+                        <SocNav {...this.props} />
+                    </div>
+
                 </div>
             </div>
         );
     }
-}
-
-Header.propTypes = {
-    location: React.PropTypes.object,
-    page: React.PropTypes.object,
 }
 
 export default Header
