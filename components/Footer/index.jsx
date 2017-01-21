24cdc159f2f7d2c2515024a85a4df9ac03db6819
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2017-01-22 00:08:16 +0200

    cleaning Footer component

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,34 +1,25 @@
 import React from 'react'
 import { prefixLink } from 'gatsby-helpers'
-import { config } from 'config'
 
 import './style.scss'
 import iconEagle from '../../static/img/svg-icons/native-american-eagle.svg'
 
 class Footer extends React.Component {
     render() {
-        const {location} = this.props
-        const {route} = this.props
-        const page = route.page.data
 
         return (
             <div className='footer'>
-                <div className='footer__copyright'>2006 - 2016 © { config.siteTitle }</div>
+                <div className='footer__copyright'>2006 - 2017 © Sentimony Records</div>
                 <div className='footer__powered'>
-                    powered by
-                    <a href='http://rooooster.com' className='footer__powered-link' target='_blank'>
+                    <span>Web Development By</span>
+                    <a href='http://ihororlovskyi.com' className='footer__powered-link' target='_blank'>
                         <img className='footer__powered-svg' src={ prefixLink(iconEagle) }/>
-                        <span className='footer__powered-fx'>Rooster Studio</span>
+                        <span className='footer__powered-fx'>Ihor Orlovskyi</span>
                     </a>
                 </div>
             </div>
         );
     }
-}
-
-Footer.propTypes = {
-    location: React.PropTypes.object,
-    page: React.PropTypes.object,
 }
 
 export default Footer
